8e51c4792993d74bf262abbdc88102cd375a6bbb
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-17 21:20:11 -0300

    Initial structure frontend

---
+++ omnistack/week 8/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>oi</h1>
+    );
+  }
+}
+
+export default App;
