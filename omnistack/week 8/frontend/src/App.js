b98f37f3f9771d23c7b9d3c8b3a91a513de3eff5
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-17 21:42:21 -0300

    View login style

--- omnistack/week 8/frontend/src/App.js
+++ omnistack/week 8/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Login from './pages/Login';
+
 class App extends Component {
   render() {
     return (
-      <h1>oi</h1>
+      <Login />
     );
   }
 }
