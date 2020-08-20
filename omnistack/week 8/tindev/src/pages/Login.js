259a14be79efa8172c981bf5d90ec86fb2a0fccc
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-19 21:14:26 -0300

    create asyncStorage on login and make handleLogin

--- omnistack/week 8/tindev/src/pages/Login.js
+++ omnistack/week 8/tindev/src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { View, StyleSheet, Image, TextInput, Text, TouchableOpacity } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({navigation}) {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user) {
+                navigation.navigate('Main', { user });
+            }
+        });
+    }, []);
+
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', { _id });
+    }
+
     return (
     <View style={styles.container}>
         <Image source={logo} />
@@ -13,9 +36,11 @@ export default function Login() {
             placeholder="Digite seu usuário do github"
             placeholderTextColor="#999"
             style={styles.input}
+            value={user}
+            onChangeText={setUser}
          />
 
-         <TouchableOpacity style={styles.button}>
+         <TouchableOpacity onPress={handleLogin} style={styles.button}>
              <Text style={styles.buttonText}>Entrar</Text>
          </TouchableOpacity>
     </View>
