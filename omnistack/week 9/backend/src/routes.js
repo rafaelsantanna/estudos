7036fee197e3c22eaefbba10cea9ba672da98d3f
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-24 20:13:27 -0300

    Configure mongoose and create Sessions Controller/Route/Model

--- omnistack/week 9/backend/src/routes.js
+++ omnistack/week 9/backend/src/routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json(req.body);
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
--- omnistack/week 9/backend/src/server.js
+++ omnistack/week 9/backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.j0rta.mongodb.net/omnistack9?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
