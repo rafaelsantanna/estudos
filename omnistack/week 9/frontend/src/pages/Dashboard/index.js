c54ed97fc678078428058518def9d42c93ad41ac
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-25 15:12:22 -0300

    Dashboard page completed, styles and handle functions

--- omnistack/week 9/frontend/src/pages/Dashboard/index.js
+++ omnistack/week 9/frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './styles.css';
 
 export default function Dashboard() {
-  return <div />
+  const [spots, setSpots] = useState([]);
+  useEffect(() => {
+    async function loadSports() {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', {
+        headers: { user_id }
+      });
+
+      setSpots(response.data);
+      
+    }
+    loadSports();
+  }, []);
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
+    </>
+  )
 }
