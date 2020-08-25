98cd88d5b9cddbe10c41467e15b0db1336c35c31
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-25 12:54:24 -0300

    styled login page form

--- omnistack/week 9/frontend/src/App.js
+++ omnistack/week 9/frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div>teste</div>
+    <div className="container">
+      <img src={logo} alt="AirCnc"/>
+
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa</p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="ser melhor e-mail"
+          />
+
+          <button className="btn" type="submit">ENTRAR</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
