6a153d0a7c520183e8be91180f05ad591b3b38f5
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-25 15:41:05 -0300

    New page style

--- omnistack/week 9/frontend/src/pages/New/index.js
+++ omnistack/week 9/frontend/src/pages/New/index.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+
+import './styles.css';
+
+import camera from '../../assets/camera.svg';
 
 export default function New() {
-  return <div />
+  const [thumbnail, setThumbnail] = useState(null);
+  const [company, setCompany] = useState('');
+  const [techs, setTechs] = useState('');
+  const [price, setPrice] = useState('');
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  });
+
+  function handleSubmit() {
+
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label 
+        id="thumbnail" 
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail? 'has-thumbail' : ''}
+      >
+        <input type="file" onChange={e =>  setThumbnail(e.target.files[0])} />
+        <img src={camera} alt="Select img"/>
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input 
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={e => setCompany(e.target.value)}
+
+      />
+
+      <label htmlFor="company">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+      <input 
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={e => setTechs(e.target.value)}
+      />
+
+      <label htmlFor="company">VALOR DA DIÁRIA * <span>em branco para GRATUITO</span></label>
+      <input 
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={e => setPrice(e.target.value)}
+      />
+
+      <button className="btn" type="submit">Cadastrar</button>
+    </form>
+  )
 }
--- omnistack/week 9/frontend/src/routes.js
+++ omnistack/week 9/frontend/src/routes.js
@@ -11,7 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/dashboard" component={Dashboard} />
-        <Route path="new" component={New} />
+        <Route path="/new" component={New} />
       </Switch>
     </BrowserRouter>
   );
