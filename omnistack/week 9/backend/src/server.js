e9f004e5c60a309dd88c4d737294f3a7742a54c3
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-28 20:17:40 -0300

    making information available to connected users throughout the project

--- omnistack/week 9/backend/src/server.js
+++ omnistack/week 9/backend/src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const socketio = require('socket.io');
-const http = require('./routes');
+const http = require('http');
 
 const routes = require('./routes');
 
@@ -12,13 +12,24 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-io.on('connection', socket => {
-  console.log('Usuário conectado', socket.id);
-});
-
 mongoose.connect('mongodb+srv://admin:admin@cluster0.j0rta.mongodb.net/omnistack9?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 app.use(cors());
