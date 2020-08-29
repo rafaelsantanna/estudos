3938f9bb71caa6d9ee62e251dcdd28cc58136b46
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-08-28 21:04:19 -0300

    approval and rejection controllers finalized

--- omnistack/week 9/backend/src/controllers/ApprovalController.js
+++ omnistack/week 9/backend/src/controllers/ApprovalController.js
@@ -1,7 +1,15 @@
-const { store } = require("./SessionController");
+const Booking = require('../models/Booking');
 
 module.exports = {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { booking_id } = req.params;
+
+    const booking = await Booking.findById(booking_id).populate('spot');
+
+    booking.approved = true;
+
+    await booking.save();
+
+    return res.json(booking);
   }
 };
