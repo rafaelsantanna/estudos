83f5a38e322eb546d4ad133a17949ef4fbd4ad34
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-11-30 08:44:01 -0300

    install and setup eslint

--- gostack11/backend/src/server.ts
+++ gostack11/backend/src/server.ts
@@ -1,10 +1,8 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!'});
-});
+app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
